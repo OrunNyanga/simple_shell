5bef193e48d20d89c5d3164759391dd5bd890f5d
Orun Nyanga <orunnyanga@gmail.com>
2022-06-15 20:00:35 -0700

    Not so simple shells can scratch you

--- main.c
+++ main.c
@@ -1,154 +1,73 @@
-@@ -0,0 +1,153 @@
-#include "shell.h"
-
-int shellby_alias(char **args, char __attribute__((__unused__)) **front);
-void set_alias(char *var_name, char *value);
-void print_alias(alias_t *alias);
+#include "main.h"
 
 /**
- * shellby_alias - Builtin command that either prints all aliases, specific
- * aliases, or sets an alias.
- * @args: An array of arguments.
- * @front: A double pointer to the beginning of args.
+ * free_data - frees data structure
  *
- * Return: If an error occurs - -1.
- *         Otherwise - 0.
+ * @datash: data structure
+ * Return: no return
  */
-int shellby_alias(char **args, char __attribute__((__unused__)) **front)
+void free_data(data_shell *datash)
 {
-	alias_t *temp = aliases;
-	int i, ret = 0;
-	char *value;
+	unsigned int i;
 
-	if (!args[0])
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		while (temp)
-		{
-			print_alias(temp);
-			temp = temp->next;
-		}
-		return (ret);
+		free(datash->_environ[i]);
 	}
-	for (i = 0; args[i]; i++)
-	{
-		temp = aliases;
-		value = _strchr(args[i], '=');
-		if (!value)
-		{
-			while (temp)
-			{
-				if (_strcmp(args[i], temp->name) == 0)
-				{
-					print_alias(temp);
-					break;
-				}
-				temp = temp->next;
-			}
-			if (!temp)
-				ret = create_error(args + i, 1);
-		}
-		else
-			set_alias(args[i], value);
-	}
-	return (ret);
+
+	free(datash->_environ);
+	free(datash->pid);
 }
 
 /**
- * set_alias - Will either set an existing alias 'name' with a new value,
- * 'value' or creates a new alias with 'name' and 'value'.
- * @var_name: Name of the alias.
- * @value: Value of the alias. First character is a '='.
- */
-void set_alias(char *var_name, char *value)
-{
-	alias_t *temp = aliases;
-	int len, j, k;
-	char *new_value;
-
-	*value = '\0';
-	value++;
-	len = _strlen(value) - _strspn(value, "'\"");
-	new_value = malloc(sizeof(char) * (len + 1));
-	if (!new_value)
-		return;
-	for (j = 0, k = 0; value[j]; j++)
-	{
-		if (value[j] != '\'' && value[j] != '"')
-			new_value[k++] = value[j];
-	}
-	new_value[k] = '\0';
-	while (temp)
-	{
-		if (_strcmp(var_name, temp->name) == 0)
-		{
-			free(temp->value);
-			temp->value = new_value;
-			break;
-		}
-		temp = temp->next;
-	}
-	if (!temp)
-		add_alias_end(&aliases, var_name, new_value);
-}
-
-/**
- * print_alias - Prints the alias in the format name='value'.
- * @alias: Pointer to an alias.
- */
-void print_alias(alias_t *alias)
-{
-	char *alias_string;
-	int len = _strlen(alias->name) + _strlen(alias->value) + 4;
-
-	alias_string = malloc(sizeof(char) * (len + 1));
-	if (!alias_string)
-		return;
-	_strcpy(alias_string, alias->name);
-	_strcat(alias_string, "='");
-	_strcat(alias_string, alias->value);
-	_strcat(alias_string, "'\n");
-
-	write(STDOUT_FILENO, alias_string, len);
-	free(alias_string);
-}
-/**
- * replace_aliases - Goes through the arguments and replace any matching alias
- * with their value.
- * @args: 2D pointer to the arguments.
+ * set_data - Initialize data structure
  *
- * Return: 2D pointer to the arguments.
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
  */
-char **replace_aliases(char **args)
+void set_data(data_shell *datash, char **av)
 {
-	alias_t *temp;
-	int i;
-	char *new_value;
+	unsigned int i;
 
-	if (_strcmp(args[0], "alias") == 0)
-		return (args);
-	for (i = 0; args[i]; i++)
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
 	{
-		temp = aliases;
-		while (temp)
-		{
-			if (_strcmp(args[i], temp->name) == 0)
-			{
-				new_value = malloc(sizeof(char) * (_strlen(temp->value) + 1));
-				if (!new_value)
-				{
-					free_args(args, args);
-					return (NULL);
-				}
-				_strcpy(new_value, temp->value);
-				free(args[i]);
-				args[i] = new_value;
-				i--;
-				break;
-			}
-			temp = temp->next;
-		}
+		datash->_environ[i] = _strdup(environ[i]);
 	}
 
-	return (args);
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
 }
 
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
+}
